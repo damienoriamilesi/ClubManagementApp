cc6d72c2a2b1343ddd8abff00ca68646538ff9a7
Damien ORIA <damien.oria@compenswiss.ch>
2020-06-24 17:45:24 +0200

    Add project files.

---
+++ ClubManagementApp.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClubManagementApp.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ ClubManagementApp/Controllers/IEntityInformation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClubManagementApp.Controllers
+{
+    public interface IEntityInformation
+    {
+        public DateTime Date { get; set; }
+        public string Summary { get; set; }
+    }
+}
