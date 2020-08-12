081bd36f96ddda94b86227f049a9df54f816edc1
Damien ORIA <damien.oria@compenswiss.ch>
2020-08-12 16:24:47 +0200

    Move IDP project location

---
+++ Company.IDP/Config.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Company.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Address(),
+                new IdentityResource("roles", "Your role(s)", new string[]{ "role" })
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("imagegalleryapi", "Image Gallery API")
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope()
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                   new Client
+                   {
+                       ClientName = "Image Gallery",
+                       ClientId = "imagegalleryapi",
+                       AllowedGrantTypes = GrantTypes.Code,
+                       RequirePkce = true,
+                       RedirectUris = new List<string>{
+                           "https://localhost:5001/signin-oidc"
+                       },
+                       AllowedScopes =
+                       {
+                           IdentityServerConstants.StandardScopes.OpenId
+                           , IdentityServerConstants.StandardScopes.Profile
+                           , IdentityServerConstants.StandardScopes.Address
+                           , "roles"
+                           //, "roles"
+                           //, "imagegalleryapi"
+                       },
+                       ClientSecrets = { new Secret("apisecret".Sha256()) }
+                   }
+            };
+    }
+}
---
+++ Company.IDP/Startup.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Company.IDP
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+            var identityServerStoreDatabaseConnectionString
+                = "Server=LAPDORIA;Database=ClubManagementIdp;Trusted_connection=True;";
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
+
+            services.AddAuthentication()
+                .AddFacebook("Facebook", opt =>
+                {
+                    opt.AppId = " ";
+                    opt.AppSecret = " ";
+                    opt.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                });
+                ////https://console.developers.google.com/
+                //.AddGoogle("Google", opt =>
+                //{
+                //    opt.ClientId = "309924201256-jklbmtk3g9vvkijjo1jhv6n5vf4p7hs3.apps.googleusercontent.com";
+                //    opt.ClientSecret = "SE_2NO1ZlegOqf_r1ipal93V";
+                //    opt.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                //})
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
